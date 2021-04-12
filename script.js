640aab99442044026c23fc4d2fa2d8d347c36647
giulianozelada22 <giulianomartinzelada@gmail.com>
2021-04-12 17:44:27 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function convert() {
+  let decimal = document.getElementById('decimal').value;
+  console.log(decimal);
+  let bin = parseInt(decimal).toString(2);
+  document.getElementById('bin').innerHTML = bin;
+  let oct = parseInt(decimal).toString(8);
+  document.getElementById('oct').innerHTML = oct;
+  let hex = parseInt(decimal).toString(16);
+  document.getElementById('hex').innerHTML = hex;
+}
+  
